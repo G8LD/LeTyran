4a8fd063e9f4f9dce33a5ca331af0bca095be362
AyoubB93 <aybouaziz@iut.univ-paris8.fr>
2022-05-20 15:05:33 +0200

    optimisation de l'affichage de la vie

--- src/main/java/application/vue/controls/VieVue.java
+++ src/main/java/application/vue/controls/VieVue.java
@@ -18,32 +18,34 @@ public class VieVue  {
     private ImageView image ;
     private Pane viePane;
 
-
+// constructeur avec placement du Pane de la vie et la taille et le placment des images
     public VieVue(StackPane root){
         this.rootPane=root;
         this.viePane=new Pane();
         this.listeVie= new Image[11];
         this.image= new ImageView(new Image("file:src/main/resources/application/Vie/10.png"));
-        this.rootPane.getChildren().add(this.viePane);
-        this.viePane.setMaxWidth(15);
-        this.viePane.setMaxHeight(10);
-        this.viePane.setLayoutX(500);
-        this.viePane.setLayoutY(100);
+        this.viePane.setMaxWidth(200);// dimension du pane
+        this.viePane.setMaxHeight(100);
+        this.viePane.setLayoutX(300);// coordonné du Pane
+        this.viePane.setLayoutY(-305);
         this.viePane.setVisible(true);
-        image.setFitHeight(50);
+        image.setFitHeight(50);//dimension de l'image
         image.setFitWidth(120);
-        image.setX(512);
-        image.setY(-312);
+        image.setX(460);// coordonnne de l'image
+        image.setY(-270);
+        this.viePane.getChildren().add(this.image);
+        this.rootPane.getChildren().add(this.viePane);
         this.afficherVie(50);
 
     }
+    // fonction pour cree une image View qui contient toute les images de la vie
     public void listeImageVie(){
         for(int i = 0; i<this.listeVie.length; i++) {
                 this.listeVie[i] = new Image("file:src/main/resources/application/Vie/" + i + ".png");
         }
     }
 
-
+// fonction qu affiche les differente image en fonction de la vie
     public void afficherVie(int pv ){
         this.listeImageVie();
 
@@ -80,8 +82,7 @@ public class VieVue  {
         else {
             image.setImage(this.listeVie[0]);
         }
-
-        this.viePane.getChildren().add(this.image);
+        
 
     }
 
