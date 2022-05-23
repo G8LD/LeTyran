84cbc115791d09b8e670adda8e6543a8b193ae71
G8LD <steven.yang.ys@gmail.com>
2022-05-23 09:30:19 +0200

    remis TUILE_TAILLE

--- src/main/java/application/modele/MapJeu.java
+++ src/main/java/application/modele/MapJeu.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 public class MapJeu {
     public final static int WIDTH = 30;
     public final static int HEIGHT = 20;
+    public final static int TUILE_TAILLE = 32;
     public final static int TAILLE_CHUNK = 25;
     public final static int DISTANCE_RENDU = 4;
 
